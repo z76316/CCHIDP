4f65400adde9b21cca8dba0b25d1eefc5c90edfa
ryh <james31435@gmail.com>
2018-05-20 01:29:22 +0800

    revise crawler-carrefour  as crawler-honestbee search different URL ok

--- crawler-python/crawler-carrefour.py
+++ crawler-python/crawler-carrefour.py
@@ -4,47 +4,33 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-url = "https://online.carrefour.com.tw/search?key=%E9%B1%88%E9%AD%9A%E9%A6%99%E7%B5%B2&categoryId="
+url = "https://online.carrefour.com.tw/search?key=%E5%86%B7%E6%B0%A3%E6%A9%9F&categoryId="
 r = requests.get(url)
-# print r.content
 
+sourceStr = r.content
+sourceStr2 = sourceStr.replace('\\', '')
+start = sourceStr2.find('"ProductListModel":[')
+end = sourceStr2.find('],"ProductIds"')
+str1 = sourceStr2[start: end]
+strList = str1.split('{"Id"')
 
-soup = BeautifulSoup(r.content)
-# print soup
+num = len(strList)
+lastIndex = num - 1
+strList2 = []
+for i in range(num):
+	if i != 0:
+		x = '{"id"' + strList[i]
 
+		if i == lastIndex:
+			x = x 
+		else:
+			x = x[:-1]
 
-# links = soup.find_all("a")
-# for link in links:
-	# print link.get("href")
-
-g_data = soup.find_all("script", {"type": "text/javascript"})
-print "**********************************************************************************************"
-for x in g_data: print x
-productInfo_bsTag = g_data[9]
-productInfo_bsNavigableString = productInfo_bsTag.string # tag to NavigableString
-productInfo_bsStr = productInfo_bsNavigableString.encode('utf8') # unicode to str
-productInfo_bsStr_remove_backslash = productInfo_bsStr.replace('\\', '') #remove \
-
-infoList = re.findall(r'\[(.*?)\]', productInfo_bsStr_remove_backslash) # retrieve product info, [str]
-info = infoList[0] # str
-
-product_str_List = info.split('},{') # get products list, [str]
-
-product_str_List_forJson = []
-for x in product_str_List:
-
-	if x[0] != '{': 
-		x = '{' + x
-
-	if x[-1] != '}': 
-		x = x + '}'
-
-	# print x
-	product_str_List_forJson.append(x)
+		strList2.append(x)
 
 
 
-product_dict_list = [json.loads(x) for x in product_str_List_forJson] #str to json
+jsonList = [json.loads(x) for x in strList2]
 
 
 
@@ -59,23 +45,24 @@ class CarrefourObject():
 # json to onject
 carrefourObject_list = [
 							CarrefourObject(
-												product_dic['Name'], 
-												product_dic['ItemQtyPerPackFormat'], 
-												product_dic['Price'], 
-												product_dic['Specification']
+												json['Name'], 
+												json['ItemQtyPerPackFormat'], 
+												json['Price'], 
+												json['Specification']
 											) 
-							for product_dic in product_dict_list
+							for json in jsonList
 						]
 
 
 
-# something = carrefourObject_list
+something = carrefourObject_list
 # print "----------------------------------------content--------------------------------------------------"
 # print something
 # print "----------------------------------------type-----------------------------------------------------"
-# print type(something)
-# print len(something)
+print type(something)
+print len(something)
 
-for carrefourObject in carrefourObject_list: 
-	print carrefourObject.Name
+for x in something:
+	print "----------------------------------------content--------------------------------------------------"
+	print x.Name, x.ItemQtyPerPackFormat, x.Price, x.Specification
 
